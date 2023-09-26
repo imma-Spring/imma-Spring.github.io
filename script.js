0c32625200086ccea4491565d62aa9638f11b6b3
imma_spring <125583307+imma-Spring@users.noreply.github.com>
2023-09-25 20:00:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/*let images = [
+    '../images/pong.svg',
+    '../images/spaceinvaders.svg'
+];
+
+// Get a random image from the array
+let image = images[Math.floor(Math.random() * images.length)%images.length];
+
+// Get the element with the class "hero"
+let item = document.querySelector("#hero");
+
+// Set the background image
+item.style.backgroundImage = `url(${image})`;*/
